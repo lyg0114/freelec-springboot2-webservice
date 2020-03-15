87961aea6ac8de09d2a3b19b9976bf57a4addb9e
iyeong-gyo <whdnseowkd@naver.com>
2020-03-15 09:53:42 +0900

    조회, 입력, 수정, 삭제 기능 구현

--- src/main/java/com/jojoldu/book/springboot/web/PostsApiController.java
+++ src/main/java/com/jojoldu/book/springboot/web/PostsApiController.java
@@ -29,4 +29,12 @@ public class PostsApiController {
         return postsService.findById(id);
     }
 
+    @DeleteMapping("/api/v1/posts/{id}")
+    public Long delte(@PathVariable Long id){
+
+        postsService.delete(id);
+        return id;
+
+    }
+
 }
